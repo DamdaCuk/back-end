77c2dd36668b853a58b9ab11534f5a9470486012
pyy2114 <pyy2114@naver.com>
2024-10-12 21:24:26 +0900

    chore: #0 프로젝트 초기 셋팅

---
+++ Damda/src/main/java/com/cuk/damda/domain/Collections.java
@@ -0,0 +1,50 @@
+package com.cuk.damda.domain;
+
+import com.cuk.damda.domain.Enum.ItemType;
+import com.cuk.damda.domain.Enum.Rating;
+import com.cuk.damda.global.domain.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name="collections_tb")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@ToString
+public class Collections extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long collectionsId;
+
+    private Long itemId;
+    private ItemType itemType;
+    private String itemTitle;
+    private String itemImg;
+
+    @Column(columnDefinition = "TEXT")
+    private String review;
+
+    private Rating rating;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "home_id", nullable = false)
+    private Home home;
+
+    @Builder
+    public Collections(Long itemId, ItemType itemType, String itemTitle, String itemImg){
+        this.itemId = itemId;
+        this.itemType = itemType;
+        this.itemTitle = itemTitle;
+        this.itemImg=itemImg;
+    }
+
+    public static Collections create(Long itemId, ItemType itemType, String itemTitle, String itemImg){
+        return Collections.builder()
+                .itemId(itemId)
+                .itemType(itemType)
+                .itemTitle(itemTitle)
+                .itemImg(itemImg)
+                .build();
+    }
+}
