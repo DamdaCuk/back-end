dbf4571a0c6a1c7bfafef570377cbed7cd01e6f3
kahyun0255 <kkh9946@naver.com>
2024-10-20 20:49:20 +0900

    fix: #2 Auditing 기능 활성화

--- Damda/src/main/java/com/cuk/damda/DamdaApplication.java
+++ Damda/src/main/java/com/cuk/damda/DamdaApplication.java
@@ -2,8 +2,10 @@ package com.cuk.damda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class DamdaApplication {
 
 	public static void main(String[] args) {
