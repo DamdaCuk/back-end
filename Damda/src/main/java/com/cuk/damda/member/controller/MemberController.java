3db01dad62ccbca45c67eaae48730037126679c0
kahyun0255 <kkh9946@naver.com>
2024-10-23 14:44:08 +0900

    refactor: #2 테스트용 코드 삭제

--- Damda/src/main/java/com/cuk/damda/member/controller/MemberController.java
+++ Damda/src/main/java/com/cuk/damda/member/controller/MemberController.java
@@ -1,19 +1,12 @@
 package com.cuk.damda.member.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api")
+@RequestMapping
 public class MemberController {
 
-    @GetMapping("/users")
-    public String getUserInfo(@AuthenticationPrincipal UserDetails userDetails) {
-        return userDetails.getUsername();
-    }
 }
