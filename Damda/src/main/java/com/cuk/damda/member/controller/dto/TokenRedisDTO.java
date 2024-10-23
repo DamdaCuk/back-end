509272e96f0c8f41d67472023c81941385e68208
kahyun0255 <kkh9946@naver.com>
2024-10-23 16:47:08 +0900

    refactor: #2 TokenRedisDTO record 형식으로 변경

--- Damda/src/main/java/com/cuk/damda/member/controller/dto/TokenRedisDTO.java
+++ Damda/src/main/java/com/cuk/damda/member/controller/dto/TokenRedisDTO.java
@@ -6,32 +6,24 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 import org.springframework.stereotype.Service;
 
-@Getter
-@Service
-@ToString
-public class TokenRedisDTO {
-    private Long userId;
-    private String refreshToken;
-    private String accessToken;
-
-    public TokenRedisDTO(Long userId, String refreshToken, String accessToken) {
-        this.userId = userId;
-        this.refreshToken = refreshToken;
-        this.accessToken = accessToken;
+public record TokenRedisDTO(
+        Long userId,
+        String refreshToken,
+        String accessToken
+) {
+    public static TokenRedisDTO of(Long userId, String refreshToken, String accessToken) {
+        return new TokenRedisDTO(userId, refreshToken, accessToken);
     }
 
-    public TokenRedisDTO() {}
-
-    public static TokenRedisDTO toDTO(TokenRedis tokenRedis){
-        TokenRedisDTO dto=new TokenRedisDTO();
-        dto.refreshToken=tokenRedis.getRefreshToken();
-        dto.userId=tokenRedis.getUserId();
-        dto.accessToken=tokenRedis.getAccessToken();
-        return dto;
+    public static TokenRedisDTO toDTO(TokenRedis tokenRedis) {
+        return new TokenRedisDTO(
+                tokenRedis.getUserId(), tokenRedis.getRefreshToken(), tokenRedis.getAccessToken()
+        );
     }
 
-    public static TokenRedis toEntity(TokenRedisDTO tokenRedisDTO){
-        TokenRedis tokenRedis=new TokenRedis(tokenRedisDTO.getRefreshToken(),tokenRedisDTO.getUserId(), tokenRedisDTO.getAccessToken());
-        return tokenRedis;
+    public static TokenRedis toEntity(TokenRedisDTO tokenRedisDTO) {
+        return new TokenRedis(
+                tokenRedisDTO.refreshToken, tokenRedisDTO.userId(), tokenRedisDTO.accessToken()
+        );
     }
 }
