b0d3df345a3327f2ed40b3d60890c40b2b7ae5d0
kahyun0255 <kkh9946@naver.com>
2024-10-29 15:43:13 +0900

    feat: #6 에러 핸들링 추가

--- Damda/src/main/java/com/cuk/damda/global/exception/GlobalExceptionHandler.java
+++ Damda/src/main/java/com/cuk/damda/global/exception/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import com.cuk.damda.config.oauth.exception.TokenException;
 import com.cuk.damda.global.controller.ApiResponse;
 import com.cuk.damda.global.exception.exceptions.DBError;
 import com.cuk.damda.global.exception.exceptions.JwtValidationException;
+import com.cuk.damda.global.exception.exceptions.UserNotFound;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.validation.BindException;
@@ -122,6 +123,19 @@ public class GlobalExceptionHandler {
         );
     }
 
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    @ExceptionHandler(UserNotFound.class)
+    public ApiResponse<Object> userNotFound(HandlerMethodValidationException e) {
+        String message = e.getAllErrors().stream()
+                .map(error -> error.getDefaultMessage())
+                .collect(Collectors.joining(", "));
+
+        return ApiResponse.of(
+                HttpStatus.UNAUTHORIZED,
+                message,
+                null
+        );
+    }
 }
 
 
