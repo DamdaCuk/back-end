38963be82dcf17b5bfb2d488173c91d2ad077fed
pyy2114 <pyy2114@naver.com>
2024-10-30 22:05:55 +0900

    feat:#8 contents repository 수정

--- Damda/src/main/java/com/cuk/damda/contents/repository/ContentsRepository.java
+++ Damda/src/main/java/com/cuk/damda/contents/repository/ContentsRepository.java
@@ -1,9 +1,13 @@
 package com.cuk.damda.contents.repository;
 
 import com.cuk.damda.contents.domain.Contents;
+import com.cuk.damda.home.domain.Home;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ContentsRepository extends JpaRepository<Contents, Long> {
+    Optional<Contents> findByItemIdAndHome(Long itemId, Home home);
 }
