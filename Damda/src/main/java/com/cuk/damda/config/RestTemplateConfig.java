bdcb3a0fd004f250546b2197c9fccf431411e3ff
kahyun0255 <kkh9946@naver.com>
2024-10-31 09:20:22 +0900

    fix: #6 RestTemplateConfig import 추가

--- Damda/src/main/java/com/cuk/damda/config/RestTemplateConfig.java
+++ Damda/src/main/java/com/cuk/damda/config/RestTemplateConfig.java
@@ -1,5 +1,7 @@
 package com.cuk.damda.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
